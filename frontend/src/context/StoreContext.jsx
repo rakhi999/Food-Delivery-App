9fdcd537a73121765eb8c1265c5923d365de0a9e
rakhi dangwal <rakhidangwal28@gmail.com>
2026-07-01 18:05:27 +0530

    Update API URL to production endpoint

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -8,7 +8,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-app-backend-6h89.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
